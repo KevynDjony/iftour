5a2cd775e7eb533ab5423d6279d2ceaa3d46a2e2
KevynDjony <k.djony@estudnte.ifmt.edu.br>
2023-07-28 09:52:03 -0300

    criado botao intent

--- app/src/main/java/com/example/iftour/MainActivity.kt
+++ app/src/main/java/com/example/iftour/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.iftour
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,16 +19,17 @@ class MainActivity : AppCompatActivity() {
         }
 
         fun inscricao(view: View){
-            val url = "https://goo.gl/maps/seV79vNLNrgfgvQw8"
-            val i = Intent(Intent.ACTION_VIEW)
-            i.data = Uri.parse(url)
-            startActivity(i)
+            val webpage: Uri = Uri.parse("https://processoseletivo.ifmt.edu.br/")
+            val intent = Intent(Intent.ACTION_VIEW, webpage)
+            startActivity(intent)
         }
-        fun localizacao(view: View){
-            val url = "https://cursosead.ifmt.edu.br/login/index.php"
-            val i = Intent(Intent.ACTION_VIEW)
-            i.data = Uri.parse(url)
-            startActivity(i)
+        fun localizacao(view: View) {
+            val gmmIntentUri = Uri.parse("geo:-15.873904088847597, -52.29794483784141")
+            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+            mapIntent.setPackage("com.google.android.apps.maps")
+            if (mapIntent.resolveActivity(packageManager) != null) {
+                startActivity(mapIntent)
+            }
         }
 
         fun horario(view: View){
