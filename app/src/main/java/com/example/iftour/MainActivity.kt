9d34bfb23015c5d22e90576f2011d994f73d2610
KevynDjony <k.djony@estudnte.ifmt.edu.br>
2023-07-28 09:52:03 -0300

    criação llayyoutt

--- app/src/main/java/com/example/iftour/MainActivity.kt
+++ app/src/main/java/com/example/iftour/MainActivity.kt
@@ -1,30 +1,27 @@
 package com.example.iftour
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-    class MainActivity : AppCompatActivity() {
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_main)
-        }
-
         fun inscricao(view: View){
-            val url = "https://goo.gl/maps/seV79vNLNrgfgvQw8"
-            val i = Intent(Intent.ACTION_VIEW)
-            i.data = Uri.parse(url)
-            startActivity(i)
+            val webpage: Uri = Uri.parse("https://processoseletivo.ifmt.edu.br/")
+            val intent = Intent(Intent.ACTION_VIEW, webpage)
+            startActivity(intent)
         }
-        fun localizacao(view: View){
-            val url = "https://cursosead.ifmt.edu.br/login/index.php"
-            val i = Intent(Intent.ACTION_VIEW)
-            i.data = Uri.parse(url)
-            startActivity(i)
+        fun localizacao(view: View) {
+            val gmmIntentUri = Uri.parse("geo:-15.873904088847597, -52.29794483784141")
+            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+            mapIntent.setPackage("com.google.android.apps.maps")
+            startActivity(mapIntent)
         }
 
         fun horario(view: View){
@@ -33,6 +30,4 @@ class MainActivity : AppCompatActivity() {
             i.data = Uri.parse(url)
             startActivity(i)
         }
-
     }
-}
